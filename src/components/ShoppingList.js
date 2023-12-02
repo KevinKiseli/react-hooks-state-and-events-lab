5e07f004eb66bfa737519e3e07128e9458cbd4b2
Kevin <kevin.kiseli@student.moringaschool.com>
2023-12-02 10:47:13 +0300

    Done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  // Initialize state for selectedCategory
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // Add an event handler to update selectedCategory when the <select> value changes
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  // Filter items based on selectedCategory
+  const filteredItems =
+    selectedCategory === "All"
+      ? items
+      : items.filter((item) => item.category === selectedCategory);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        {/*Connect the state and event handler to the <select> element */}
+        <select
+          name="filter"
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+        >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +32,8 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {/* Map over the filteredItems instead of items */}
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -22,3 +42,4 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
